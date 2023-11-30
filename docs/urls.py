cf54eddbb98ea23ea58ea3a114262d87d19a7574
Anirban Mishra <anirban.mishra@weavers-web.com>
2023-11-30 19:02:39 +0530

    Authentication & Permissions

--- docs/urls.py
+++ docs/urls.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from rest_framework.urlpatterns import format_suffix_patterns
 from snippets.views import * 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('snippets', SnippetList.as_view(), name = "snippet_list"),
-    path('snippets/<int:pk>', SnippetDetail.as_view(), name = "snippet_list_by_id"),
+    path('snippets/', SnippetList.as_view(), name = "snippet_list"),
+    path('snippets/<int:pk>/', SnippetDetail.as_view(), name = "snippet_list_by_id"),
+    path('users/', UserList.as_view()),
+    path('users/<int:pk>/', UserDetail.as_view()),
 ]
+
+urlpatterns += [
+    path('api-auth/', include('rest_framework.urls')),
+]
+#  api-auth/login
+#  api-auth/register
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- snippets/views.py
+++ snippets/views.py
@@ -1,12 +1,28 @@
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer,UserSerializer
 from rest_framework import mixins
 from rest_framework import generics
+from django.contrib.auth.models import User
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from snippets.permissions import IsOwnerOrReadOnly
 
 class SnippetList(generics.ListCreateAPIView):
+    permission_classes=[IsAuthenticatedOrReadOnly]
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
     
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes=[IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
