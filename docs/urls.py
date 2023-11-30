78bc48a436671cff60a5493ea88a137e132f2f33
Anirban Mishra <anirban.mishra@weavers-web.com>
2023-11-30 16:16:09 +0530

    Class Based -

--- docs/urls.py
+++ docs/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from snippets.views import * 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('custom', checkUtils , name = "cutomm"),
-    path('snippets', snippet_list, name = "snippet_list"),
-    path('snippets/<int:pk>', snippet_detail, name = "snippet_list_by_id"),
+    path('snippets', SnippetList.as_view(), name = "snippet_list"),
+    path('snippets/<int:pk>', SnippetDetail.as_view(), name = "snippet_list_by_id"),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
